891e12d481cdad38f7e62ab116a351f6bbc41301
Elvinius <elwinabbasli@gmail.com>
2021-02-28 11:07:36 +0200

    Implement SPA Part1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
-import { DISHES } from '../shared/dishes';
-import DishDetail from './DishDetailComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
+import Contact from './ContactComponent'
 import { Switch, Route, Redirect } from 'react-router-dom';
+import { DISHES } from '../shared/dishes';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
+import { COMMENTS } from '../shared/comments';
 
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
             dishes: DISHES,
-
+            comments: COMMENTS,
+            promotions: PROMOTIONS,
+            leaders: LEADERS
         }
     }
 
@@ -20,7 +25,9 @@ class Main extends Component {
     render() {
         const HomePage = () => {
             return (
-                <Home />
+                <Home dish={this.state.dishes.filter(dish => dish.featured)[0]}
+                    promotion={this.state.promotions.filter(promo => promo.featured)[0]}
+                    leader={this.state.leaders.filter(leader => leader.featured)[0]} />
             )
         }
         return (
@@ -29,6 +36,7 @@ class Main extends Component {
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route exact path="/contactus" component={Contact} />
                     <Redirect to="/home" />
                 </Switch>
                 <Footer />
