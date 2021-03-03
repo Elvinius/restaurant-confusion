25215901b9a70c89e64f51e98f4b044a4e584b33
Elvinius <elwinabbasli@gmail.com>
2021-03-03 15:31:22 +0200

    Implement thunk and logger

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,33 +1,45 @@
 import React from 'react'
 import { Card, CardImg, CardText, CardBody, CardSubtitle, CardTitle } from 'reactstrap';
+import { Loading } from './LoadingComponent';
 
-const RenderCard = ({ item }) => {
-    return (
-        <Card>
-            <CardImg src={item.image} alt={item.name} />
-            <CardBody>
-                <CardTitle>{item.name}</CardTitle>
-                {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null}
-                <CardText>{item.description}</CardText>
-            </CardBody>
-        </Card>
-    )
+const RenderCard = ({ item, isLoading, errMessage }) => {
+    if (isLoading) {
+        return (
+            <Loading />
+        )
+    }
+    else if (errMessage) {
+        return (
+            <h4>{errMessage}</h4>
+        )
+    } else {
+        return (
+            <Card>
+                <CardImg src={item.image} alt={item.name} />
+                <CardBody>
+                    <CardTitle>{item.name}</CardTitle>
+                    {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null}
+                    <CardText>{item.description}</CardText>
+                </CardBody>
+            </Card>
+        )
+    }
 }
 
-const Home = ({ dish, leader, promotion }) => {
+const Home = (props) => {
     return (
         <div className="container">
             <div className="row align-items-start">
                 <div className="col-12 col-md m-1">
-                    <RenderCard item={dish} />
+                    <RenderCard item={props.dish} isLoading={props.dishesLoading} errMessage={props.dishesErrMessage} />
 
                 </div>
                 <div className="col-12 col-md m-1">
-                    <RenderCard item={promotion} />
+                    <RenderCard item={props.promotion} />
 
                 </div>
                 <div className="col-12 col-md m-1">
-                    <RenderCard item={leader} />
+                    <RenderCard item={props.leader} />
                 </div>
             </div>
         </div>
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -4,6 +4,7 @@ import {
     CardTitle, Breadcrumb, BreadcrumbItem
 } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
 
 const RenderMenuItem = ({ dish }) => {
     return (
@@ -19,7 +20,7 @@ const RenderMenuItem = ({ dish }) => {
 
 }
 const Menu = (props) => {
-    const menu = props.dishes.map(
+    const menu = props.dishes.dishes.map(
         dish => {
             return (
                 <div key={dish.id} className="col-12 col-md-5 m-1">
@@ -27,24 +28,42 @@ const Menu = (props) => {
                 </div>
             );
         });
-    return (
-        <div className="container">
-            <div className="row">
-                <Breadcrumb>
-                    <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
-                    <BreadcrumbItem active>Menu</BreadcrumbItem>
-                </Breadcrumb>
-                <div className="col-12">
-                    <h3>Menu</h3>
-                    <hr />
+
+    if (props.dishes.isLoading) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Loading />
                 </div>
             </div>
-            <div className="row">
-                {menu}
+        )
+    } else if (props.dishes.errMessage) {
+        return (
+            <div className="container">
+                <div className="row">
+                    <h4>{props.dishes.errMessage}</h4>
+                </div>
             </div>
-        </div>
-    );
-
+        )
+    } else {
+        return (
+            <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>Menu</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>Menu</h3>
+                        <hr />
+                    </div>
+                </div>
+                <div className="row">
+                    {menu}
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Menu;
